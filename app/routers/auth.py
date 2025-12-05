ec127a69c7872a98ff47684ff885293129c73232
U007U <119404723+U007U@users.noreply.github.com>
2025-12-05 20:17:42 +0300

    fix: add OAuth2PasswordBearer for Swagger Bearer auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer,  OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from datetime import datetime, timedelta
@@ -9,6 +9,8 @@ from app.database import get_db
 from app.models import User
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
