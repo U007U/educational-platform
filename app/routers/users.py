dc09d0564581ac690447b17f60c2d9673b723d21
U007U <119404723+U007U@users.noreply.github.com>
2025-12-12 16:46:08 +0300

    Add users and lessons APIs, dedicated test DB and status updates

--- app/routers/users.py
+++ app/routers/users.py
@@ -5,6 +5,7 @@ from typing import List
 from ..database import get_db
 from ..models import User
 from .. import schemas
+from .auth import get_password_hash
 
 
 router = APIRouter(prefix="/users", tags=["users"])
@@ -12,19 +13,19 @@ router = APIRouter(prefix="/users", tags=["users"])
 
 @router.post("/", response_model=schemas.UserRead)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    # В будущем здесь будет хэширование пароля.
-    fake_hashed_password = "fake-hash-" + user.password
+    hashed_password = get_password_hash(user.password)
 
     db_user = User(
         email=user.email,
         full_name=user.full_name,
-        hashed_password=fake_hashed_password,
+        hashed_password=hashed_password,
         role=user.role,
     )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 
 @router.get("/", response_model=List[schemas.UserRead])
